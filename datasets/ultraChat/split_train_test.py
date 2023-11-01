e185120400543b8ca3947c94e592ca9898e44c68
mz <35124505+michael-wzhu@users.noreply.github.com>
2023-11-01 23:23:42 +0800

    1017

--- datasets/ultraChat/split_train_test.py
+++ datasets/ultraChat/split_train_test.py
@@ -3,7 +3,7 @@ import random
 if __name__ == "__main__":
 
     list_examples = []
-    for i in range(3):
+    for i in [0, ]:
         list_examples.extend(
             open(f"datasets/ultraChat/train_{i}.jsonl", "r", encoding="utf-8").readlines()
         )
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     random.shuffle(list_examples)
 
     with open(f"datasets/ultraChat/train.json", "w", encoding="utf-8") as f:
-        for samp in list_examples[: -1500]:
+        for samp in list_examples[: 5000]:
             f.write(samp)
     with open(f"datasets/ultraChat/test.json", "w", encoding="utf-8") as f:
-        for samp in list_examples[-1500 : ]:
+        for samp in list_examples[-500 : ]:
             f.write(samp)
 
